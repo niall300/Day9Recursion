589deebb9c556a6a492d6f1ec1d144b4d0e150fc
niall coffey <niall300@yahoo.co.uk>
2017-04-18 22:54:05 +0100

    Factorial using recursion

---
+++ Day9Recursion/Solution.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+class Solution
+{
+    static void Main(String[] args)
+    {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. 
+         * Your class should be named Solution */
+        int input = Int32.Parse(Console.ReadLine());
+        int output = Factorial(input);
+        Console.WriteLine(output);
+    }
+    static int Factorial(int i)
+    {
+        int factorial;
+        if (i <= 1)
+        {
+            factorial = 1;
+        }
+        else
+        {
+            factorial = i * Factorial(i - 1);
+        }
+        return factorial;
+
+    }
+}
